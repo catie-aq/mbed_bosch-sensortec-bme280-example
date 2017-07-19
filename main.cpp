30ba0b6298e653be95c5d933ad6e87cd2a7fd59b
Nabil Elqatib <n.elqatib@catie.fr>
2017-07-19 11:27:33 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@
 #include "bme280.h"
 
 namespace {
-#define PERIOD_MS 2000
+#define PERIOD_MS 4000
 }
 
 static I2C i2c(I2C_SDA, I2C_SCL);
@@ -29,38 +29,54 @@ static double humidity;
 static BME280::SensorMode mode;
 
 void read_print_data(){
+    bme.take_forced_measurement();
     bme.read_temperature(&temp);
     bme.read_humidity(&humidity);
     bme.read_pressure(&pressure);
 
     printf("Temperature: %.3f °C\n", temp);
-    printf("Pressure:    %.3f Pa\n", pressure);
-    printf("Himidity:   %.3f %%\n", humidity);
+    printf("Pressure:    %.3f hPa\n", (pressure / 100.0f));
+    printf("Humidity:    %.3f %%\n", humidity);
 }
 
 int main() {
-
+#if 1
     if (!bme.initialize()){
-        printf("Couldn't initialize the BME280...\n=======================\n");
+        printf("Couldn't initialize the BME280...\n");
         return -1;
     }
+
+    bme.set_sampling(BME280::SensorMode::FORCED,
+           BME280::SensorSampling::OVERSAMPLING_X1,
+           BME280::SensorSampling::OVERSAMPLING_X1,
+           BME280::SensorSampling::OVERSAMPLING_X1,
+           BME280::SensorFilter::OFF,
+           BME280::StandbyDuration::MS_1000);
     
+    bme280_settings_t settings = bme.get_settings();
+    printf("Settings:\n\tP_os: %d\n\tT_os: %d\n\tH_os: %d\n\tfilter:%d\n\tstandby: %d\n\t",
+            settings.osrs_p, settings.osrs_t, settings.osrs_h, settings.filter, settings.standby_time);
+
     bme.get_power_mode(&mode);
     while (true){
 
         printf("\nPower mode: "); 
         switch (static_cast<int>(mode)){
-        case 0:
+        case 1:
             printf("Forced\n");
             break;
-        case 1:
+        case 0:
             printf("Sleep\n");
             break;
-        default:
+        case 3:
             printf("Normal\n");
             break;
+        default:
+        	printf("Error\n");
+        	break;
         }
         read_print_data();
         wait_ms(PERIOD_MS);
     }
+#endif
 }
